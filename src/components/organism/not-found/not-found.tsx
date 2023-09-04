5e99bbc0d9b5e760eba3cd45edaa064e27cc4f51
Kristian Korneev <krisnero@yandex.ru>
2023-09-04 23:42:44 +0300

    - improve 404

--- src/components/organism/not-found/not-found.tsx
+++ src/components/organism/not-found/not-found.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Card} from "@/components/molecules/card/card";
 import './not-found.css';
+import Image from "next/image";
 
 export default function Custom404() {
   return (
@@ -9,9 +10,13 @@ export default function Custom404() {
       customContentClass="notFound__container"
     >
       {`Look's like page that are you looking for not exists, please follow next link to go to the home page`}
-      <p>
+      <div className="notFound__imageContainer">
+        <Image width={200} height={200} src="./404-error.png"/>
+      </div>
+      <p className="notFound__linkContainer">
         <a className="link" href="https://kristiankorneev.github.io/kris-login-page/">{` Go to the home page`}</a>
       </p>
+
     </Card>
   )
 }
